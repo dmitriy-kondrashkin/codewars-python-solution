73e057312126ae790b4c4e8e73b02da656d63209
dmitriy-kondrashkin <dimon8967@mail.ru>
2023-05-01 23:37:32 +0300

    Solution for task: Get the Middle Character

--- 7KYU/get_the_middle_character.py
+++ 7KYU/get_the_middle_character.py
@@ -1,2 +1,14 @@
 def get_middle(s):
-    pass
+    if 0 < len(s) < 1000:
+        if len(s) % 2 == 0:
+            middle = int(len(s) / 2)
+            result = f'{s[middle-1]}{s[middle]}'
+        elif len(s) % 2 != 0:
+            middle = int(len(s) / 2)
+            result = f'{s[middle]}'
+    return result
+
+
+some_str = 'testing'
+
+print(get_middle(some_str))
