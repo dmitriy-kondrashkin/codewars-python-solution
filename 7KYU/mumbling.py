5b286d645bdc0006429a026ae745874bcf2ef569
dmitriy-kondrashkin <dimon8967@mail.ru>
2023-05-02 00:14:53 +0300

    Solution for task: Mumbling

--- 7KYU/mumbling.py
+++ 7KYU/mumbling.py
@@ -1,3 +1,12 @@
 def accum(s):
-    # your code
-    pass
+    result = []
+    count = 0
+    for letter in s:
+        count += 1
+        output = str(letter*count).capitalize()
+        result.append(output)
+    return '-'.join(result)
+
+
+some_string = 'RqaEzty'
+print(accum(some_string))
