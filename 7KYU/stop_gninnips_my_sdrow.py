d5621bb6fcdff90a490f84e154dae6c550bc17ae
dmitriy-kondrashkin <dimon8967@mail.ru>
2023-05-02 17:14:57 +0300

    Solution for task: Stop gninnipS My sdroW!

---
+++ 7KYU/stop_gninnips_my_sdrow.py
@@ -0,0 +1,6 @@
+def spin_words(sentence):
+    return ' '.join([word[::-1] if len(word) >= 5 else word for word in sentence.split()])
+
+
+sentence = "Hey fellow warriors"
+print(spin_words(sentence))
