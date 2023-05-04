378dfa4913a0372f87ccbce5941aecc86339897d
dmitriy-kondrashkin <dimon8967@mail.ru>
2023-05-04 12:26:07 +0300

    Solution for task: Simple Pig Latin

---
+++ 6KYU/simple_pig_latin.py
@@ -0,0 +1,7 @@
+def pig_it(text):
+    return ' '.join([word[1:]+word[:1] + 'ay' if word.isalpha() else word for word in text.split()])
+
+text_1 = 'Pig latin is cool'                # igPay atinlay siay oolcay
+text_2 = 'Hello world !'                    # elloHay orldway ! 
+text_3 = "Quis custodiet ipsos custodes ?"  # uisQay ustodietcay psosiay ustodescay ? 
+print(pig_it(text_3))
