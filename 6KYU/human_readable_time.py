cb7d81cd60eb193af94213840085b175d8fc2890
dmitriy-kondrashkin <dimon8967@mail.ru>
2023-05-04 15:41:39 +0300

    Solution for task: Human Readable Time

---
+++ 6KYU/human_readable_time.py
@@ -0,0 +1,23 @@
+# Solution 1
+
+def make_readable(seconds):
+    h = seconds // 3600
+    m = (seconds - h * 3600) // 60
+    s = seconds - (h * 3600) - (m * 60)
+    return f'{h:0>2}:{m:0>2}:{s:0>2}'
+
+
+# Solution 2
+
+def make_redable(seconds):
+    return '{:02}:{:02}:{:02}'.format(seconds//3600, seconds//60%60, seconds%60)
+
+
+# HH = hours, padded to 2 digits, range: 00 - 99
+# MM = minutes, padded to 2 digits, range: 00 - 59
+# SS = seconds, padded to 2 digits, range: 00 - 59
+
+seconds_1 = 5                   #     --> "00:00:05"
+seconds_2 = 60                  #     --> "00:01:00"
+seconds_3 = 86399               #     --> "23:59:59"
+print(make_readable(seconds_2))
