fd6a817d191b2ceb60dcebfe691627d8f063f3b4
dmitriy-kondrashkin <dimon8967@mail.ru>
2023-05-04 18:28:57 +0300

    Solution for task: RGB To Hex Conversion

---
+++ 5KYU/rgb_to_hex_conversion.py
@@ -0,0 +1,11 @@
+def rgb(r, g, b):
+    return f'{max(0, min(r, 255)):02x}{max(0, min(g, 255)):02x}{max(0, min(b, 255)):02x}'.upper()
+
+# rgb(255, 255, 255)    # returns FFFFFF
+# rgb(255, 255, 300)    # returns FFFFFF
+# rgb(0,0,0)            # returns 000000
+# rgb(148, 0, 211)      # returns 9400D3
+
+
+r, g, b = -20, 275, 125
+print(rgb(r, g, b))
